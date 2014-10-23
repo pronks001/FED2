8d465b73c5bfeb14ad77dc9ad31efe32e93ababd
pronks001 <pronks001@gmail.com>
2014-10-23 16:32:46 +0200

    eindopdracht FED2
    steeds een stukje dichterbij, underscore-min.js aan de praat gekregen
    maar nog niet kunnen inladen in de html....

--- Opdrachten/WebApp/static/js/app.js
+++ Opdrachten/WebApp/static/js/app.js
@@ -1,37 +1,12 @@
-
+// NameSpace of Module pattern
 var app = app || {};
 
-var xhr = {
-	trigger: function (type, url, success, data) {
-		var req = new XMLHttpRequest;
-		req.open(type, url, true);
-
-		req.setRequestHeader('Content-type','application/json');
-
-		type === 'POST' ? req.send(data) : req.send(null);
-
-		req.onreadystatechange = function() {
-			if (req.readyState === 4) {
-				if (req.status === 200 || req.status === 201) {
-					success(req.responseText);
-				}
-			}
-		}
-	}
-};
-
-function succes (text){
-	console.log(JSON.parse(text));
-}
-
-xhr.trigger("GET", "http://dennistel.nl/movies", succes, "JSON");
-
-(function(){ 
+// Self Invoking Anonymous Function
+(function(){
 
 	app.controller = {
-
-		init:function(){
-			// app.working.init();
+		init: function(){
+			app.config.init();
 			app.router.init();
 			app.sections.init();
 		}
@@ -39,124 +14,165 @@ xhr.trigger("GET", "http://dennistel.nl/movies", succes, "JSON");
 
 	app.router = {
 
-		init:function(){
-
+		init: function() {
 			routie({
-				'about':function() {
-					console.log("about");
-					app.sections.toggle("#about");
-				},
-				'movies':function(){
-					console.log("movies");
-					app.sections.toggle("#movies");
-
-				}
+			    'about': function() {
+			    	console.log("About");
+					app.sections.toggle("about");
+			    },
+			    'movies': function() {
+			    	console.log("Movies");
+					app.sections.toggle("movies");
+			    }
 			});
-
 		}
+
 	}
 
 	app.content = {
 
 		about: {
 			title: "About this app",
-			description:[
-			{
-				p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at finibus diam. Sed commodo."
-			},
-			{
-				p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at finibus diam. Sed commodo."
-			},
-			{
-				p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at finibus diam. Sed commodo."
-			},
-			{
-				p: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce at finibus diam. Sed commodo."
-			},
-
+			description: [
+				{
+					paragraph: "Cities fall but they are rebuilt. heroes die but they are remembered. the man likes to play chess; let's get him some rocks. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. bruce... i'm god. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all. rehabilitated? well, now let me see. you know, i don't have any idea what that means. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. rehabilitated? well, now let me see. you know, i don't have any idea what that means. cities fall but they are rebuilt. heroes die but they are remembered. no, this is mount everest. you should flip on the discovery channel from time to time. but i guess you can't now, being dead and all."
+				},
+				{
+					paragraph: "I did the same thing to gandhi, he didn't eat for three weeks. bruce... i'm god. cities fall but they are rebuilt. heroes die but they are remembered. i once heard a wise man say there are no perfect men. only perfect intentions. cities fall but they are rebuilt. heroes die but they are remembered. boxing is about respect. getting it for yourself, and taking it away from the other guy. well, what is it today? more spelunking? let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. bruce... i'm god. well, what is it today? more spelunking? it only took me six days. same time it took the lord to make the world. i did the same thing to gandhi, he didn't eat for three weeks."
+				},
+				{
+					paragraph: "Let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. boxing is about respect. getting it for yourself, and taking it away from the other guy. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. you measure yourself by the people who measure themselves by you. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. circumstances have taught me that a man's ethics are the only possessions he will take beyond the grave. you measure yourself by the people who measure themselves by you. you measure yourself by the people who measure themselves by you. that tall drink of water with the silver spoon up his ass. i once heard a wise man say there are no perfect men. only perfect intentions. mister wayne, if you don't want to tell me exactly what you're doing, when i'm asked, i don't have to lie. but don't think of me as an idiot. boxing is about respect. getting it for yourself, and taking it away from the other guy."
+				},
+				{
+					paragraph: "That tall drink of water with the silver spoon up his ass. well, what is it today? more spelunking? i now issue a new commandment: thou shalt do the dance. let me tell you something my friend. hope is a dangerous thing. hope can drive a man insane. i did the same thing to gandhi, he didn't eat for three weeks. the man likes to play chess; let's get him some rocks. i now issue a new commandment: thou shalt do the dance. i now issue a new commandment: thou shalt do the dance. multiply your anger by about a hundred, kate, that's how much he thinks he loves you. i don't think they tried to market it to the billionaire, spelunking, base-jumping crowd. that tall drink of water with the silver spoon up his ass. it only took me six days. same time it took the lord to make the world."
+				}
 			]
 		},
 
-		movies:{
-			title: "Favorite movies",
-			movieContent: [
+		movieTitle :{title:"Movies"},
 
-    		{	title:'Shawshank Redemption',
-    			releaseDate:'14 October 1994',
-    			description:'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    			cover:'static/images/shawshank-redemption.jpg'
-    		},
-
-    		{	title:'TheGodfather',
-    			releaseDate:'14 October 1994',
-    			description:'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    			cover:'static/images/the-godfather.jpg'
-    		},
-
-    		{	title:'PulpFiction',
-    			releaseDate:'14 October 1994',
-    			description:'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    			cover:'static/images/pulp-fiction.jpg'
-    		},
-
-    		{	title:'TheDarkKnight',
-    			releaseDate:'14 October 1994',
-    			description:'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    			cover:'static/images/the-dark-knight.jpg'
-    		}
-		]
-
-		}
+		movieReviews: {},
+		movies: {}
 	}
 
-	app.directives = {
-		cover: {
-			src: function(params) {
-				return this.cover;
-			}
-		}
-	};
+	app.sections = {
 
-	app.sections = { 
-		init: function(){ 
-			app.sections.about(); 
+		init: function() {
+			app.sections.about();
 			app.sections.movies();
+			app.sections.toggle();
+			app.sections.manipulateData();
+		},
 
-			//app.sections.toggle(); 
-		}, 
-		about: function(){ 
-			Transparency.render(document.getElementById('about'), app.content.about); 
-		}, 
-		movies: function(){ 
-			movies: {
-				Transparency.render(document.getElementById('movies'), app.content.movies);
+		about: function() {
+			Transparency.render(document.getElementById('about'), app.content.about);
+		},
+
+		movies: function() {
+			var self = this;
+
+			if(localStorage.getItem('movieData')){
+				Transparency.render(document.getElementById('movies'), JSON.parse(localStorage.getItem('movieData')), app.config.directives);
+				Transparency.render(document.getElementById('movie'), app.content.movieTitle, app.config.directives);
+			}
+			else{
+							app.config.xhr.trigger("GET", "http://dennistel.nl/movies", self.moviesSucces, "JSON");
+
+			}
+
+			
+
+
+
+		},
+
+		moviesSucces: function(text) {
+			console.log('Parsed data', JSON.parse(text));
+			app.content.movies = JSON.parse(text);
+			console.log('Data from data object', app.content.movies);
+			
+			//underscore_js
+
+
+			Transparency.render(document.getElementById('movies'), app.content.movies, app.config.directives);
+			Transparency.render(document.getElementById('movie'), app.content.movieTitle, app.config.directives);
+
+			localStorage.setItem('movieData', text);
+		},
+
+		manipulateData: function() {
+
+            var data = JSON.parse(localStorage.getItem('movieData'));
+
+            //map reduce
+            _.map(data, function (movie, i){
+                    movies.reviews   = _.reduce(movie.reviews,   function(memo, review){   return memo + review.score; }, 0) / movie.reviews.length;
+                    //movie.directors = _.reduce(movie.directors, function(memo, director){ return memo + director.name + ' '; }, '');
+                    //movie.actors    = _.reduce(movie.actors,    function(memo, actor){    return memo + actor.actor_name + ', ';}, '');
+                    //return movie;
+                    console.log(movies.reviews)
+                })  
+            return data;
+        },
+
+		toggle: function(section) {
+			if (section == "about") {
+				document.querySelector('#about').classList.add('active');
+				document.querySelector('#movie').classList.remove('active');
+				document.querySelector('#movies').classList.remove('active');
 			} 
-
-			moviesContent: {
-				Transparency.render(document.getElementsByClassName('moviesContent')[0], app.content.movies.movieContent, app.directives); 
-			}
-		}, 
-
-
-		toggle: function(sections){
-
-			if (sections == "#about") {
-				document.getElementById('about' ).classList.add( 'active' );
-				document.getElementById('movies' ).classList.remove( 'active'); 
-				//document.querySelector("#movies").classList.remove('active'); 
-			}
-
-			else if (sections == "#movies"){ 
-				document.getElementById('movies').classList.add('active');
-				document.getElementById('about' ).classList.remove('active'); 
+			else if (section == "movies") {
+				document.querySelector('#movie').classList.add('active');
+				document.querySelector('#movies').classList.add('active');
+				document.querySelector('#about').classList.remove('active');
 			}
 		}
-	};
 
+	}
+
+	app.config = {
+		init: function() {
+            this.transparency();
+        },
+
+        // Custom binding name
+        transparency: function() {
+            Transparency.matcher = function(element, key) {
+                return element.el.getAttribute('data-name') == key;
+            };
+        },
+
+		directives: {
+			cover: {
+			    src: function(params) {
+			      	return this.cover;
+			    }
+		  	}
+		},
+
+		xhr: {
+			trigger: function (type, url, success, data) {
+				var req = new XMLHttpRequest;
+
+				req.open(type, url, true);
+
+				req.setRequestHeader('Content-type','application/json');
+
+				type === 'POST' ? req.send(data) : req.send(null);
+
+				req.onreadystatechange = function() {
+					if (req.readyState === 4) {
+						if (req.status === 200 || req.status === 201) {
+							success(req.responseText);
+						}
+					}
+				}
+			}
+		}
+
+	}
 
 
 })();
 
 app.controller.init();
-
-
